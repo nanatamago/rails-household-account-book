c4a06925234f2b6a6227e86005fa90510d8bf94f
nanatamago <mc.std.0510@gmail.com>
2022-03-27 13:36:21 +0000

    feat: add a function to total

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,8 @@ class BooksController < ApplicationController
     
     def index
         @books = Book.all
+        @books = @books.where(year: params[:year]) if params[:year].present?
+        @books = @books.where(month: params[:month]) if params[:month].present?
     end
     def show
     end
@@ -10,10 +12,9 @@ class BooksController < ApplicationController
         @book = Book.new
     end
     def create
-        book_params = params.require(:book).permit(:year,:month,:inout,:category,:amount)
         @book = Book.new(book_params)
         if @book.save
-            flash[:notice] = "データを登録しました"
+            flash[:notice] = @book.year.to_s + "年" + @book.month.to_s + "月" + @book.category + "を登録しました"
            redirect_to books_path
         else
             flash.now[:alert] = "登録に失敗しました"
@@ -23,7 +24,6 @@ class BooksController < ApplicationController
     def edit
     end
     def update
-        book_params = params.require(:book).permit(:year,:month,:inout,:category,:amount)
         if @book.update(book_params)
             flash[:notice] = "データを登録しました"
             redirect_to book_path(@book)
@@ -42,5 +42,8 @@ class BooksController < ApplicationController
     
     def set_book
         @book = Book.find(params[:id])
+    end
+    def book_params
+        book_params = params.require(:book).permit(:year,:month,:inout,:category,:amount)
     end
 end
