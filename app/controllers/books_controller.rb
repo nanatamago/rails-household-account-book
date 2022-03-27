4f8865bd6acfba413cac30540dabe6fe361584d6
nanatamago <mc.std.0510@gmail.com>
2022-03-27 11:30:38 +0000

    refactor: controller,routes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,10 @@
 class BooksController < ApplicationController
+    before_action :set_book, only:[:show,:edit,:update,:destroy]
+    
     def index
         @books = Book.all
     end
     def show
-        @book = Book.find(params[:id])
     end
     def new
         @book = Book.new
@@ -12,26 +13,34 @@ class BooksController < ApplicationController
         book_params = params.require(:book).permit(:year,:month,:inout,:category,:amount)
         @book = Book.new(book_params)
         if @book.save
+            flash[:notice] = "データを登録しました"
            redirect_to books_path
         else
+            flash.now[:alert] = "登録に失敗しました"
             render:new
         end
     end
     def edit
-        @book = Book.find(params[:id])
     end
     def update
-        @book = Book.find(params[:id])
         book_params = params.require(:book).permit(:year,:month,:inout,:category,:amount)
         if @book.update(book_params)
+            flash[:notice] = "データを登録しました"
             redirect_to book_path(@book)
         else
+            flash.now[:alert] = "登録に失敗しました"
             render:edit
         end
     end
     def destroy
-        @book = Book.find(params[:id])
         @book.destroy
+        flash[:notice] = "削除しました"
         redirect_to books_path
+    end
+    
+    private 
+    
+    def set_book
+        @book = Book.find(params[:id])
     end
 end
